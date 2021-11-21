029a091e6218f48533624b8d95043d8c8dbecda9
Betül Ön <betulon97@gmail.com>
2021-11-21 21:40:00 +0300

    player walking update

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Vector2 moveInput;
+    Rigidbody2D myRigidbody;
+    [SerializeField] float walkSpeed = 5f;
+
+    Animator myAnimator;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody2D>();
+        myAnimator =GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Walk();
+        FlipSprite();
+    }
+
+    void OnMove(InputValue value)
+    { 
+        moveInput = value.Get<Vector2>();
+    }
+    void Walk()
+    {
+        Vector2 playerVelocity = new Vector2(moveInput.x * walkSpeed, myRigidbody.velocity.y);
+        myRigidbody.velocity = playerVelocity;
+
+        bool playerHasHorizontalSpeed = Mathf.Abs(myRigidbody.velocity.x) > Mathf.Epsilon;
+        myAnimator.SetBool("isWalking", playerHasHorizontalSpeed);
+        
+    }
+
+    void FlipSprite()
+    {
+        bool playerHasHorizontalSpeed = Mathf.Abs(myRigidbody.velocity.x) > Mathf.Epsilon;
+
+        if (playerHasHorizontalSpeed)
+        {
+            transform.localScale = new Vector2(Mathf.Sign(myRigidbody.velocity.x), 1);
+        }
+    }
+}
