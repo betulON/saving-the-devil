0f6fcc6fd1edc454cec97b36f976b329df2176de
betulOn <betulon97@gmail.com>
2021-11-23 15:26:58 +0300

    enemy movement updated

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -1,18 +1,32 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] float moveSpeed;
+    Rigidbody2D myRigidbody;
+
     void Start()
     {
-        
+        myRigidbody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Walk();
+        FlipSprite();
+    }
+
+    void Walk()
+    {
+        myRigidbody.velocity = new Vector2(moveSpeed, 0f);
+    }
+
+    void FlipSprite()
+    {
+        transform.localScale = new Vector2((-1) * Mathf.Sign(myRigidbody.velocity.x), 1f);
     }
 }
