abd70f320811a7c20d052e310e9f61fd033b9995
betulOn <betulon97@gmail.com>
2021-11-26 10:05:19 +0300

    female fae movement fix

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -6,29 +6,35 @@ using UnityEngine;
 public class EnemyMovement : MonoBehaviour
 {
     [SerializeField] float moveSpeed = 1f;
+    [SerializeField] float minDistanceToFollow = 0.2f;
+    [SerializeField] float maxDistanceToFollow = 15f;
+
     Rigidbody2D myRigidbody;
     PlayerMovement player;
+    Animator myAnimator;
 
     void Start()
     {
         myRigidbody = GetComponent<Rigidbody2D>();
         player = FindObjectOfType<PlayerMovement>();
+        myAnimator = GetComponent<Animator>();
     }
 
     void Update()
     {
+        float horizontalDistance = PlayerHorizontalDistance();
         Walk();
     }
 
     void Walk()
     {
         float horizontalDistance = PlayerHorizontalDistance();
-        if (horizontalDistance < 2f)
+        if (horizontalDistance < minDistanceToFollow)
         {
             myRigidbody.velocity = new Vector2(0f, 0f);
             return;
         }
-        if (horizontalDistance < 10f)
+        if (horizontalDistance < maxDistanceToFollow)
         {
             moveSpeed = moveSpeed * Mathf.Sign(player.transform.position.x - transform.position.x) * Mathf.Sign(moveSpeed);
         }
